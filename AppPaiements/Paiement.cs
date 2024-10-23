e81f1eb5f67c61f4127a173510adda500b4246fb
Christ <jeanchristkoko23@gmail.com>
2024-10-23 13:23:17 -0400

    classe Paiement

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        public Paiement(double montant, string description)
+        {
+            Montant = montant;
+            Description = description;
+        }
+
+        public virtual void Afficherdetails()
+        {
+            Console.WriteLine($"le paiement a {Description} est de ${Montant}");
+        }
+    }
+}
