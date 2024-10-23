873b96fb43dd453fe5eb4db339a4530b5af35fcd
Christ <jeanchristkoko23@gmail.com>
2024-10-23 13:35:50 -0400

    ajout de cartecredit

---
+++ AppPaiements/CarteCredit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit(double montant, string description, int Carte) : base(montant, description)
+        {
+            NumeroCarte = Carte;
+        }
+
+        public override void Afficherdetails()
+        {
+            Console.WriteLine($"La carte de credit {NumeroCarte} a ete debiter de ${Montant} pour {Description}");
+        }
+    }
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using AppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CarteCredit achat1 = new CarteCredit(300, "epicerie", 4534);
+        CarteCredit achat2 = new CarteCredit(40, "Mcdo", 4509);
+        CarteCredit achat3 = new CarteCredit(1500, "Prada", 4534);
+
+        achat1.Afficherdetails();
+        achat2.Afficherdetails();
+        achat3.Afficherdetails();
     }
 }
