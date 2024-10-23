23d4e779373274c8c14ba966da69a6583a017c71
Christ <jeanchristkoko23@gmail.com>
2024-10-23 13:51:51 -0400

    ajout d'utilisateur

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> paiements;
+
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            paiements = new List<Paiement>();
+        }
+
+        public void AjouterPaiement(Paiement paiement)
+        {
+            paiements.Add(paiement);
+        }
+
+        public void AfficherInfo()
+        {
+            Console.WriteLine($"Informations sur les paiements de l'utilisateur : {Nom}");
+            foreach (var paiement in paiements)
+            {
+                paiement.Afficherdetails();
+            }
+        }
+    }
+}
